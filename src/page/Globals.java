27d3d98a2b7b3e328535bcd4f276dd77287842e1
Eva Rahmadanti <eva.rahmadanti@mitrais.com>
2022-04-08 08:59:03 +0700

    Initial commit

---
+++ src/page/Globals.java
@@ -0,0 +1,6 @@
+package page;
+
+public class Globals {
+    public static String chromeDriverName = "webdriver.chrome.driver";
+    public static String chromeDriverPath = "webdriver\\chromedriver.exe";
+}
